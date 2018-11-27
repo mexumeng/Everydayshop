4bf8a3ad9166f44b348a8d4a29f13c808bee13b3
xumengmeng <599597576@qq.com>
2018-11-27 14:45:28 +0800

    通过serializers将goods直接序列化成json格式

--- apps/goods/views_base.py
+++ apps/goods/views_base.py
@@ -21,16 +21,20 @@ class GoodsListView(View):
         #     json_dict['category'] = good.category.name
         #     json_dict['market_price'] = good.market_price
         #     json_list.append(json_dict)
-        from django.forms.models import model_to_dict
+        # from django.forms.models import model_to_dict
         """
         通过model_to_dict将good所有的字段提取转化成dict类型,
         问题是imgaeFiled和DateFIled无法序列化成json格式，下面用serialize替换此方法"""
-        for good in goods:
-            json_dict = model_to_dict(good)
-            json_list.append(json_dict)
+        # for good in goods:
+        #     json_dict = model_to_dict(good)
+        #     json_list.append(json_dict)
+        from django.core import serializers
+        '''
+        通过serializers将goods直接序列化成json格式
+        '''
         import json
         from django.http import HttpResponse
-        json_data = json.dumps(json_list)
+        json_data = serializers.serialize("json", goods)
         return HttpResponse(json_data, content_type='application/json')
 
 
