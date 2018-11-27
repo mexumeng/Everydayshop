8e0e4b4873633e7e2a41b6e697a8605c9eb35b7f
xumengmeng <599597576@qq.com>
2018-11-27 18:05:06 +0800

    使用mixins.ListModelMixin,generics.GenericAPIView的第一种实现view

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -3,25 +3,23 @@ from .serializers import GoodsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 
 from .models import Goods
 
 # Create your views here.
 
 
-class GoodsListView(APIView):
+class GoodsListView(mixins.ListModelMixin,generics.GenericAPIView):
     """
-    商品列表展示页数据
+    商品列表页
     """
-    def get(self, request, format=None):
-        goods = Goods.objects.all()[:10]
-        serializer = GoodsSerializer(goods, many=True)
-        return Response(serializer.data)
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
 
-    # def post(self, request, format=None):
-    #     serializer = GoodsSerializer(data=request.date)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data,status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
 
